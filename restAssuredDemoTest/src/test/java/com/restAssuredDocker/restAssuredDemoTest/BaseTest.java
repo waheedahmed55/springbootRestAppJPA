9ca36a1cb1a4b35c08c881cf055778af0deba01e
Waheed Ahmed <waheedahmed55@gmail.com>
2019-07-10 11:55:18 -0400

    updated the Basetest calss to point to apprest-service name

--- restAssuredDemoTest/src/test/java/com/restAssuredDocker/restAssuredDemoTest/BaseTest.java
+++ restAssuredDemoTest/src/test/java/com/restAssuredDocker/restAssuredDemoTest/BaseTest.java
@@ -16,7 +16,7 @@ public class BaseTest {
 	public void setUp() {
 		
 		builder = new RequestSpecBuilder();
-		builder.setBaseUri("http://localhost:8081");
+		builder.setBaseUri("http://apprest-service:8081");
 		validRequest = builder.build();
 	}
 	
